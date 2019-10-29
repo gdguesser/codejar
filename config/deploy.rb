dadbd5ad9a8b026dd809ba6810463eb92b2ac4cd
Gabriel Guesser <50717407+gdguesser@users.noreply.github.com>
2019-10-29 10:58:14 -0300

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.2"
 
 set :application, "Codejar"
-set :repo_url, "https://github.com/gdguesser/ror-with-devise.git"
+set :repo_url, "git@github.com:gdguesser/ror-with-devise.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
